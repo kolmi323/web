5d9002342516394dadf07c2e7689997045963c54
kolmi <kolmi323@mail.ru>
2025-02-05 20:05:00 +0300

    write first test for DAO layer - UserDAOTest

--- src/main/java/ru/sql/get_user_accounts.sql
+++
@@ -1,4 +0,0 @@
-SELECT u.*, a.name, a.balance
-FROM users as u
-JOIN account as a on u.id = a.user_id
-WHERE u.id = ?
--- src/main/java/ru/sql/get_user_transaction_by_date.sql
+++
@@ -1,5 +0,0 @@
-SELECT u.*, a.name, a.balance, t.from_account_id, t.to_account_id, t.amount, t.date
-FROM users as u
-JOIN account as a on u.id = a.user_id
-JOIN transaction as t on a.id = t.from_account_id OR a.id = t.to_account_id
-WHERE u.id = ? AND t.date = CURRENT_DATE - 1
