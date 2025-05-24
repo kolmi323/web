4cb8468e4441bdf8fcda361af53e0577855b0145
kolmi <kolmi323@mail.ru>
2025-05-24 20:36:33 +0300

    implement information controller

--- src/main/java/ru/gnezdilov/web/controller/personal/categorytransaction/CategoryTransactionController.java
+++ src/main/java/ru/gnezdilov/web/controller/personal/categorytransaction/CategoryTransactionController.java
@@ -4,27 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.gnezdilov.service.dto.UserDTO;
 import ru.gnezdilov.service.personal.UserService;
+import ru.gnezdilov.web.interfaces.InformationController;
 import ru.gnezdilov.web.interfaces.SecureController;
 import ru.gnezdilov.web.json.navigation.NavigationRequest;
 import ru.gnezdilov.web.json.navigation.NavigationResponse;
 
 @Service("/report")
 @RequiredArgsConstructor
-public class CategoryTransactionController implements SecureController<NavigationRequest, NavigationResponse> {
+public class CategoryTransactionController implements InformationController<NavigationResponse> {
     private final UserService userService;
 
     @Override
-    public NavigationResponse handle(NavigationRequest request, int userId) {
+    public NavigationResponse handle(int userId) {
         UserDTO userDTO = userService.getUserById(userId);
         if (userDTO != null) {
             String[] navigationItems = {"/incoming", "/outgoing"};
             return new NavigationResponse("report page", userDTO.getName(), navigationItems);
         }
         return null;
-    }
-
-    @Override
-    public Class<NavigationRequest> getRequestClass() {
-        return NavigationRequest.class;
     }
 }
--- src/main/java/ru/gnezdilov/web/controller/personal/transaction/TransactionController.java
+++ src/main/java/ru/gnezdilov/web/controller/personal/transaction/TransactionController.java
@@ -4,27 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.gnezdilov.service.dto.UserDTO;
 import ru.gnezdilov.service.personal.UserService;
+import ru.gnezdilov.web.interfaces.InformationController;
 import ru.gnezdilov.web.interfaces.SecureController;
 import ru.gnezdilov.web.json.navigation.NavigationRequest;
 import ru.gnezdilov.web.json.navigation.NavigationResponse;
 
 @Service("/transaction")
 @RequiredArgsConstructor
-public class TransactionController implements SecureController<NavigationRequest, NavigationResponse> {
+public class TransactionController implements InformationController<NavigationResponse> {
     private final UserService userService;
 
     @Override
-    public NavigationResponse handle(NavigationRequest request, int userId) {
+    public NavigationResponse handle(int userId) {
         UserDTO userDTO = userService.getUserById(userId);
         if (userDTO != null) {
             String[] navigationItems = {"/add"};
             return new NavigationResponse("transaction page", userDTO.getName(), navigationItems);
         }
         return null;
-    }
-
-    @Override
-    public Class<NavigationRequest> getRequestClass() {
-        return NavigationRequest.class;
     }
 }
