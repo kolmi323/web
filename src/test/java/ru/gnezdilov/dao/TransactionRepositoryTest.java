468f16af4392a75cb856bb9d40a5c172bd468881
Anton Gnezdilov <gar.postgateg@gmail.com>
2025-10-20 19:33:04 +0300

    add test on dao

---
+++ src/test/java/ru/gnezdilov/dao/TransactionRepositoryTest.java
@@ -0,0 +1,57 @@
+package ru.gnezdilov.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.gnezdilov.WebApplication;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = WebApplication.class)
+public class TransactionRepositoryTest {
+    @Autowired
+    private TransactionRepository subj;
+
+    private List<Object[]> transactions;
+
+    @Before
+    public void setUp() throws Exception {
+        transactions = new ArrayList<>();
+        Object[] firstElement = {"no type", new BigDecimal("600.00")};
+        Object[] secondElement = {"hobby", new BigDecimal("500.00")};
+        transactions.add(firstElement);
+        transactions.add(secondElement);
+    }
+
+    @Test
+    public void getIncomingTransaction_returnListObject_whenCalledWithValidArgument() {
+        List<Object[]> incomingTransactions = subj.getIncomingTransaction(1,
+                LocalDate.of(2025, 1, 1),
+                LocalDate.of(2025, 12, 31));
+        assertNotNull(incomingTransactions);
+        assertEquals(transactions.get(0), incomingTransactions.get(0));
+        assertEquals(transactions.get(1), incomingTransactions.get(1));
+    }
+
+    @Test
+    public void getOutgoingTransaction_returnListObject_whenCalledWithValidArgument() {
+        List<Object[]> incomingTransactions = subj.getOutgoingTransaction(1,
+                LocalDate.of(2025, 1, 1),
+                LocalDate.of(2025, 12, 31));
+        assertNotNull(incomingTransactions);
+        assertEquals(transactions.get(0), incomingTransactions.get(0));
+        assertEquals(transactions.get(1), incomingTransactions.get(1));
+    }
+}
